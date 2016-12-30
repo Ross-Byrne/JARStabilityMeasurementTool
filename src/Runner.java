f52523427601b396b5766b117183a1b52534c0f0
Ross Byrne <ross.byrne@outlook.ie>
2016-12-30 15:44:09 +0000

    Fixed Project

--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,11 @@
 /**
  * Created by Ross Byrne on 30/12/16.
  */
-
-// main Runner class
 public class Runner {
 
     public static void main(String[] args) {
+
+        System.out.println("Hey.");
 
     } // main()
 
