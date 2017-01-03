7861e17300b6b18f2f0b74a5c22bd8bcdf198e3f
Ross Byrne <ross.byrne@outlook.ie>
2017-01-03 16:28:59 +0000

    Updated the layout of the main window

--- src/ie/gmit/sw/AppWindow.java
+++ src/ie/gmit/sw/AppWindow.java
@@ -16,28 +16,49 @@ public class AppWindow {
 		//Create a window for the application
 		frame = new JFrame();
 		frame.setTitle("JAR Stability Measurement Tool");
-		frame.setSize(550, 500);
+		frame.setSize(600, 500);
 		frame.setResizable(false);
 		frame.setLayout(new FlowLayout());
 		
         //The file panel will contain the file chooser
-        JPanel top = new JPanel(new FlowLayout(FlowLayout.LEADING));
+        JPanel top = new JPanel(new FlowLayout(FlowLayout.CENTER));
         top.setBorder(new javax.swing.border.TitledBorder("Select a JAR File"));
-        top.setPreferredSize(new Dimension(500, 100));
-        top.setMaximumSize(new Dimension(500, 100));
-        top.setMinimumSize(new Dimension(500, 100));
-        
-        final JTextField txtFileName =  new JTextField(20);
-		txtFileName.setPreferredSize(new Dimension(100, 30));
-		txtFileName.setMaximumSize(new Dimension(100, 30));
-		txtFileName.setMargin(new Insets(2, 2, 2, 2));
+        top.setPreferredSize(new Dimension(560, 140));
+        top.setMaximumSize(new Dimension(560, 140));
+        top.setMinimumSize(new Dimension(560, 140));
+
+        // Create panel to hold text box with file path
+        JPanel fileNamePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        fileNamePanel.setPreferredSize(new Dimension(500, 60));
+        fileNamePanel.setMaximumSize(new Dimension(500, 60));
+        fileNamePanel.setMinimumSize(new Dimension(500, 60));
+
+        // create panel to hold buttons
+        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        buttonsPanel.setPreferredSize(new Dimension(500, 40));
+        buttonsPanel.setMaximumSize(new Dimension(500, 40));
+        buttonsPanel.setMinimumSize(new Dimension(500, 40));
+
+        // create a label for text input
+        final JLabel filePathLabel = new JLabel("Selected JAR file path:");
+        filePathLabel.setHorizontalTextPosition(SwingConstants.LEFT);
+        filePathLabel.setPreferredSize(new Dimension(490, 20));
+        filePathLabel.setMaximumSize(new Dimension(400, 20));
+        filePathLabel.setMinimumSize(new Dimension(100, 20));
+
+        // create text field for selected file
+        final JTextField txtFileName =  new JTextField(44);
+		txtFileName.setPreferredSize(new Dimension(400, 30));
+		txtFileName.setMaximumSize(new Dimension(400, 30));
+		txtFileName.setMargin(new Insets(4, 2, 2, 2));
 		txtFileName.setMinimumSize(new Dimension(100, 30));
-		
+
+		// create button for choosing jar file
 		JButton btnChooseFile = new JButton("Browse...");
 		btnChooseFile.setToolTipText("Select JAR File");
         btnChooseFile.setPreferredSize(new Dimension(90, 30));
         btnChooseFile.setMaximumSize(new Dimension(90, 30));
-        btnChooseFile.setMargin(new Insets(2, 2, 2, 2));
+        btnChooseFile.setMargin(new Insets(4, 2, 2, 2));
         btnChooseFile.setMinimumSize(new Dimension(90, 30));
 		btnChooseFile.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
@@ -51,12 +72,13 @@ public class AppWindow {
             	}
 			}
         });
-		
+
+		// button for doing something
 		JButton btnOther = new JButton("Do Something");
 		btnOther.setToolTipText("Do Something");
 		btnOther.setPreferredSize(new Dimension(150, 30));
 		btnOther.setMaximumSize(new Dimension(150, 30));
-		btnOther.setMargin(new Insets(2, 2, 2, 2));
+		btnOther.setMargin(new Insets(4, 2, 2, 2));
 		btnOther.setMinimumSize(new Dimension(150, 30));
 		btnOther.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
@@ -64,33 +86,41 @@ public class AppWindow {
             	
 			}
         });
-		
-        top.add(txtFileName);
-        top.add(btnChooseFile);
-        top.add(btnOther);
+
+        // add components to JPanel
+        fileNamePanel.add(filePathLabel);
+        fileNamePanel.add(txtFileName);
+        buttonsPanel.add(btnChooseFile);
+        buttonsPanel.add(btnOther);
+
+        // add panels to top panel
+        top.add(fileNamePanel);
+        top.add(buttonsPanel);
         frame.getContentPane().add(top); //Add the panel to the window
-        
-        
+
         //A separate panel for the programme output
-        JPanel mid = new JPanel(new FlowLayout(FlowLayout.LEADING));
+        JPanel mid = new JPanel(new FlowLayout(FlowLayout.CENTER));
         mid.setBorder(new BevelBorder(BevelBorder.RAISED));
-        mid.setPreferredSize(new Dimension(500, 300));
-        mid.setMaximumSize(new Dimension(500, 300));
-        mid.setMinimumSize(new Dimension(500, 300));
-        
-        CustomControl cc = new CustomControl(new Dimension(500, 300));
+        mid.setPreferredSize(new Dimension(560, 250));
+        mid.setMaximumSize(new Dimension(560, 250));
+        mid.setMinimumSize(new Dimension(560, 250));
+
+        // custom control
+        CustomControl cc = new CustomControl(new Dimension(540, 230));
         cc.setBackground(Color.WHITE);
-        cc.setPreferredSize(new Dimension(300, 300));
-        cc.setMaximumSize(new Dimension(300, 300));
-        cc.setMinimumSize(new Dimension(300, 300));
+        cc.setPreferredSize(new Dimension(300, 230));
+        cc.setMaximumSize(new Dimension(300, 230));
+        cc.setMinimumSize(new Dimension(300, 230));
         mid.add(cc);
 		frame.getContentPane().add(mid);
-		
+
+		// add panel to bottom of window
         JPanel bottom = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        bottom.setPreferredSize(new Dimension(500, 50));
-        bottom.setMaximumSize(new Dimension(500, 50));
-        bottom.setMinimumSize(new Dimension(500, 50));
-        
+        bottom.setPreferredSize(new Dimension(560, 50));
+        bottom.setMaximumSize(new Dimension(560, 50));
+        bottom.setMinimumSize(new Dimension(560, 50));
+
+        // create button for showing dialog
         JButton btnDialog = new JButton("Show Dialog"); //Create Quit button
         btnDialog.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
@@ -99,17 +129,23 @@ public class AppWindow {
 
 			}
         });
-        
+
+        // create quit application button
         JButton btnQuit = new JButton("Quit"); //Create Quit button
         btnQuit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
             	System.exit(0);
 			}
         });
+
+        // add buttons to bottom panel
         bottom.add(btnDialog);
         bottom.add(btnQuit);
 
-        frame.getContentPane().add(bottom);       
+        // add bottom panel to window
+        frame.getContentPane().add(bottom);
+
+        // make window visible
 		frame.setVisible(true);
 
 	} // constructor
